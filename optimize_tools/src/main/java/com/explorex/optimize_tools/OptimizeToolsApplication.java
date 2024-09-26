1e2960d95f976253baec978b7546878bfdb560d3
Placidoe <2036640041@qq.com>
2024-09-26 21:12:40 +0800

    init project

---
+++ optimize_tools/src/main/java/com/explorex/optimize_tools/OptimizeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.explorex.optimize_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizeToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimizeToolsApplication.class, args);
+    }
+
+}
